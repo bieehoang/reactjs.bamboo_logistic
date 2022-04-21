9532744a188aa5fe43d66f405f5471c9cac52cac
qiese <hoangtrungbi2002@gmail.com>
2022-04-21 12:02:36 +0700

    React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Bamboo Logistic</a>
+      </header>
+      <main>HEllO</main>
+    </div>
+  );
+}
+
+export default App;
