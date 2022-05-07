36b475f9f7551f52407c7ef7c46d64cc5fbdbc6c
qiese <hoangtrungbi2002@gmail.com>
2022-05-08 00:24:44 +0700

    master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,35 @@
-import { BrowserRouter, Link } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import { Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreens';
 import ProductScreen from './screens/ProductScreen';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer } from 'react-router-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <div>
-          <header>
-            <Navbar bg="dark" variant="dark">
-              <Container>
-                <LinkContainer to="/">
-                  <Navbar.Brand>Bamboo Logistic</Navbar.Brand>
-                </LinkContainer>
-              </Container>
-            </Navbar>
-          </header>
-        </div>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Bamboo Logistic</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center" bg="dark">
+            Copyright by Trung Quy
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
