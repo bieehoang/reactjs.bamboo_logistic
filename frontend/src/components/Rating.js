5c6cb234b8688aa86a1b9fe6b31b724234f37eeb
qiese <hoangtrungbi2002@gmail.com>
2022-05-10 00:41:17 +0700

    Master

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,54 +1,64 @@
-function Ratings(props) {
+function Rating(props) {
   const { rating, numReviews } = props;
-  <div className="rating">
-    <span>
-      <i
-        className={
-          rating >= 1
-            ? 'fas fa-star'
-            : rating >= 0.5
-            ? 'fas fa-star-half-alt'
-            : 'far fa-star'
-        }
-      />
-      <i
-        className={
-          rating >= 2
-            ? 'fas fa-star'
-            : rating >= 1.5
-            ? 'fas fa-star-half-alt'
-            : 'far fa-star'
-        }
-      />
-      <i
-        className={
-          rating >= 3
-            ? 'fas fa-star'
-            : rating >= 2.5
-            ? 'fas fa-star-half-alt'
-            : 'far fa-star'
-        }
-      />
-      <i
-        className={
-          rating >= 4
-            ? 'fas fa-star'
-            : rating >= 3.5
-            ? 'fas fa-star-half-alt'
-            : 'far fa-star'
-        }
-      />
-      <i
-        className={
-          rating >= 5
-            ? 'fas fa-star'
-            : rating >= 4.5
-            ? 'fas fa-star-half-alt'
-            : 'far fa-star'
-        }
-      />
-    </span>
-    <span>{numReviews} reviews</span>
-  </div>;
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            rating >= 1
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 2
+              ? 'fas fa-star'
+              : rating >= 1.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 3
+              ? 'fas fa-star'
+              : rating >= 2.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 4
+              ? 'fas fa-star'
+              : rating >= 3.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 5
+              ? 'fas fa-star'
+              : rating >= 4.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span> {numReviews} reviews</span>
+    </div>
+  );
 }
-export default Ratings;
+export default Rating;
